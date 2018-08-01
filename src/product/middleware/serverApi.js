ec32f1a19e51e49fd261a08f4044f5bb6cfd31d8
zhy0302 <2290235411@qq.com>
2018-08-01 19:15:38 +0800

    -m8/1

--- src/product/middleware/serverApi.js
+++ src/product/middleware/serverApi.js
@@ -1,21 +1,75 @@
-import axios from 'axios';
-import * as ActionType from '../ActionType';
+ import axios from 'axios';
+ import * as ActionType from '../ActionType';
+{/*} const callServerApi=(endpoint,params)=>{
+//   axios({
+//     method: 'POST',
+//     url:"http://xly-wkop.xiaoniangao.cn/getUserInfo",      
+//     headers: {'Content-Type': 'application/x-www-form-urlencoded'}, 
+//     data:params
+//   }).then(res => {
+//     console.log(res);
+//     next({
+//       type: `${type}_SUC`,
+//       data: res.data
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     next({
+//       type: `${type}_FAI`
+//     });
+//   });
+// }
+// export default store=>next=>action=>{
+//     if(!action.SERVER_API){
+//         console.log('hhhhhh')
+//         return next(action);
+//     }
+//     const{
+//         type,
+//         url,
+//         params
+//     }=action.SERVER_API
+//     console.log(type)
+//     console.log(url)
+//     console.log(params)
+//     if(typeof type!='string')
+//     {throw newError("type should be a string")};
+//     if(typeof endpoint!='string')
+//     {throw newError("type should be a string")};
+//     if(typeof params!='string')
+//     {throw newError("type should be a string")};
+//     return callServerApi(endpoint,params)
+//     .then(res => {
+//     console.log(res);
+//     next({
+//       type: `${type}_SUC`,
+//       data: res.data
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     next({
+//       type: `${type}_FAI`
+//     });
+//   });*/}
+
 export default store=>next=>action=>{
-    if(!action.SERVER_API){
-        console.log('hhhhhh')
-        return next(action);
-    }
-    const{
-        type,
-        url,
-        params
-    }=action.SERVER_API
-    console.log(type)
-    console.log(url)
-    console.log(params)
-// next({
-//         type: `${type}_REQ`,
-//       });
+  if(!action.SERVER_API){
+      console.log('hhhhhh')
+      return next(action);
+  }
+  const{
+      type,
+      url,
+      params
+  }=action.SERVER_API
+  console.log(type)
+  console.log(url)
+  console.log(params)
+   next({
+         type: `${type}_REQ`,
+       });
 axios({
     method: 'POST',
     url:"http://xly-wkop.xiaoniangao.cn/getUserInfo",      
